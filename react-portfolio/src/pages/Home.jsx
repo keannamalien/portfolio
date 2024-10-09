a164d17c80ca4aef640472f6ff8edf140dad3317
keanna <keannamalien@gmail.com>
2024-10-09 02:38:30 -0700

    worked on homepage html (header and works section)

--- react-portfolio/src/pages/Home.jsx
+++ react-portfolio/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import Loading from '../utilities/Loading'
 import { restBase } from '../utilities/Utilities'
+import { Link } from 'react-router-dom'
+
 
 import React from 'react'
 
@@ -33,29 +35,70 @@ const Home = () => {
         <>
             {isLoaded ?
                 <>
-                    <section>
-                        <h1>{restDataPage.acf.name}</h1>
-                        <p>{restDataPage.acf.title}</p>
+                    {/* Header Section */}
+                    <header>
+                        <nav>
+                            <ul>
+                                <li><Link to="#works">Works</Link></li>
+                                <li><Link to="#about">About</Link></li>
+                                <li><Link to="#contact">Contact</Link></li>
+                            </ul>
+                        </nav>
+                        <section>
+                            <h1>{restDataPage.acf.name}</h1>
+                            <p>{restDataPage.acf.title}</p>
+                            <div>
+                                <p>{restDataPage.acf.tagline}</p>
+                                <p>{restDataPage.acf.tagline_2}</p>
+                            </div>
+                        </section>
+                    </header>
 
-                        <div>
-                            <p>{restDataPage.acf.tagline}</p>
-                            <p>{restDataPage.acf.tagline_2}</p>
+
+                    {/* Works Section */}
+                    <section id="works">
+                        <h2>Works</h2>
+                        <div className="work-gallery">
+                            <article className="work-item">
+                                <Link>
+                                    <img src={restDataPosts[0].acf.work_image[0].url} alt={restDataPosts[0].title.rendered} />
+                                    <h3>{restDataPosts[0].title.rendered}</h3>
+                                    {/* <p className="tools-used">
+                                    <span>React</span> |
+                                    <span>Adobe XD</span> |
+                                    <span>Photoshop</span>
+                                </p> */}
+                                </Link>
+                            </article>
+
+                            <article className="work-item">
+                                <Link>
+                                    <img src={restDataPosts[1].acf.work_image[0].url} alt={restDataPosts[1].title.rendered} />
+                                    <h3>{restDataPosts[1].title.rendered}</h3>
+                                    {/* <p className="tools-used">
+                                    <span>HTML</span> |
+                                    <span>CSS</span> |
+                                    <span>JavaScript</span>
+                                </p> */}
+                                </Link>
+                            </article>
+
+                            <article className="work-item">
+                                <Link>
+                                    <img src={restDataPosts[2].acf.work_image[0].url} alt={restDataPosts[2].title.rendered} />
+                                    <h3>{restDataPosts[2].title.rendered}</h3>
+                                    {/* <p className="tools-used">
+                                    <span>HTML</span> |
+                                    <span>CSS</span> |
+                                    <span>JavaScript</span>
+                                </p> */}
+                                </Link>
+                            </article>
                         </div>
-
+                        <p>You can also check out my <Link to="#">architectural work</Link>.</p>
                     </section>
 
-                    <section>
-                        <div>
-                            <img src={restDataPosts[2].acf.work_image[0].url} alt="image1" />
-                            <p>{restDataPosts[2].title.rendered}</p>
 
-                        </div>
-                        <img src={restDataPosts[2].acf.work_image[1].url} alt="image2" />
-                        {restDataPosts[1].title.rendered}
-                        <img src={restDataPosts[2].acf.work_image[2].url} alt="image3" />
-                        {restDataPosts[0].title.rendered}
-
-                    </section>
                 </>
                 :
                 <Loading />
